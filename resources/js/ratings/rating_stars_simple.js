5bcd365e6a7c03ef324559315945ed36e4ef10cc
artur <artur.1996.harutyunyan@gmail.com>
2023-09-04 09:51:59 +0400

    Fixed rating in main page

--- resources/js/ratings/rating_stars_simple.js
+++ resources/js/ratings/rating_stars_simple.js
@@ -1,5 +1,6 @@
 const ratingGroup = document.querySelector('#half-stars-example .rating-group');
 const ratingLabels = ratingGroup.querySelectorAll('.rating__label');
+let rate;
 
 ratingLabels.forEach((label, index) => {
     label.addEventListener('mouseover', () => {
@@ -8,44 +9,31 @@ ratingLabels.forEach((label, index) => {
         labelsBeforeTarget.forEach(label => label.classList.add('hovered'));
     });
 
-    ratingGroup.addEventListener('mouseout', resetShowRating);
+    if (rate === undefined) {
+        ratingGroup.addEventListener('mouseout', resetShowRating);
+    }
 
-    label.addEventListener('mousedown', handleSendRating)
+    label.addEventListener('mousedown', handleRating)
 });
 
-function resetShowRating() {
-    ratingLabels.forEach(label => label.classList.remove('hovered'));
+function handleRating(evt) {
+    rate = evt.target.parentElement.attributes["aria-label"].nodeValue;
+
+    ratingLabels.forEach(function (label) {
+        if (label.getAttribute('aria-label') <= rate) {
+            label.classList.add('hovered')
+        }
+    })
 }
 
-function handleSendRating(evt) {
-    let rate = evt.target.parentElement.attributes["aria-label"].nodeValue;
-    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+function resetShowRating(evt) {
+    ratingLabels.forEach(label => label.classList.remove('hovered'));
 
-    // fetch(`/`, {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'X-CSRF-TOKEN': csrfToken,
-    //     },
-    //     body: JSON.stringify({
-    //         rating_value: rate
-    //     }),
-    // })
-    //   .then(response => response.text())
-    //   .then(data => {
-    //       let block = document.getElementById(`rating_${}`)
-    //       block.innerHTML = data;
-    //
-    //       const labels = document.querySelectorAll('.rating__label')
-    //       labels.forEach(function (label) {
-    //           if (label.getAttribute('aria-label') <= rate) {
-    //               label.classList.add('hovered')
-    //           }
-    //       })
-    //
-    //   })
-    //   .catch(error => {
-    //       alert('Something went wrong')
-    //       console.error('Error:', error);
-    //   });
+    ratingLabels.forEach(function (label) {
+        if (label.getAttribute('aria-label') <= rate) {
+            label.classList.add('hovered')
+        } else {
+            label.classList.remove('hovered')
+        }
+    })
 }
