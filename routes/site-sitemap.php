3e611b58ce0b376be0a23120ba9c2edee28f8afd
alexandr017 <sasha17@live.ru>
2023-09-25 11:21:17 +0300

    xml-sitemap

--- routes/site-sitemap.php
+++ routes/site-sitemap.php
@@ -1,8 +1,19 @@
 <?php
 
-// sitemap
+
+// sitemap (html)
 Route::get('sitemap', [App\Http\Controllers\Site\V3\SItemap\HTML\HTMLSitemapController::class, 'index']);
 Route::get('sitemap/kursy/{alias}', [App\Http\Controllers\Site\V3\SItemap\HTML\HTMLSitemapController::class, 'coursesCategories']);
 
 
 // schools
+
+// sitemap (xml)
+Route::get('sitemap.xml', [App\Http\Controllers\Site\V3\SItemap\XML\XMLSitemapController::class, 'index']);
+Route::get('xml-style.xsl', [App\Http\Controllers\Site\V3\SItemap\XML\XMLSitemapController::class, 'style']);
+Route::get('pages.xml', [App\Http\Controllers\Site\V3\SItemap\XML\XMLSitemapController::class, 'pages']);
+Route::get('posts-categories.xml', [App\Http\Controllers\Site\V3\SItemap\XML\XMLSitemapController::class, 'postsCategories']);
+Route::get('posts.xml', [App\Http\Controllers\Site\V3\SItemap\XML\XMLSitemapController::class, 'posts']);
+Route::get('employees.xml', [App\Http\Controllers\Site\V3\SItemap\XML\XMLSitemapController::class, 'employees']);
+Route::get('companies.xml', [App\Http\Controllers\Site\V3\SItemap\XML\XMLSitemapController::class, 'companies']);
+Route::get('listings.xml', [App\Http\Controllers\Site\V3\SItemap\XML\XMLSitemapController::class, 'listings']);
