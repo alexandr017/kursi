07b9d69c269a6962b069ebc39181919e4dd199ad
artur <artur.1996.harutyunyan@gmail.com>
2023-11-21 11:22:21 +0400

    Removed / from sitemap

--- resources/views/site/sitemap-xml/index.blade.php
+++ resources/views/site/sitemap-xml/index.blade.php
@@ -2,22 +2,22 @@
 <?php echo '<?xml-stylesheet type="text/xsl" href="/xml-style.xsl"?>'."\n"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
-        <loc>{{\Request::getSchemeAndHttpHost()}}/pages.xml/</loc>
+        <loc>{{\Request::getSchemeAndHttpHost()}}/pages.xml</loc>
     </sitemap>
     <sitemap>
-        <loc>{{\Request::getSchemeAndHttpHost()}}/posts-categories.xml/</loc>
+        <loc>{{\Request::getSchemeAndHttpHost()}}/posts-categories.xml</loc>
     </sitemap>
     <sitemap>
-        <loc>{{\Request::getSchemeAndHttpHost()}}/posts.xml/</loc>
+        <loc>{{\Request::getSchemeAndHttpHost()}}/posts.xml</loc>
     </sitemap>
     <sitemap>
-        <loc>{{\Request::getSchemeAndHttpHost()}}/employees.xml/</loc>
+        <loc>{{\Request::getSchemeAndHttpHost()}}/employees.xml</loc>
     </sitemap>
     <sitemap>
-        <loc>{{\Request::getSchemeAndHttpHost()}}/companies.xml/</loc>
+        <loc>{{\Request::getSchemeAndHttpHost()}}/companies.xml</loc>
     </sitemap>
     <sitemap>
-        <loc>{{\Request::getSchemeAndHttpHost()}}/listings.xml/</loc>
+        <loc>{{\Request::getSchemeAndHttpHost()}}/listings.xml</loc>
     </sitemap>
 </sitemapindex>
 <!-- XML Sitemap -->
