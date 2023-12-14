da0c7e660a460ad89ccb2796561251e4c9145f75
arkadii-morozov <arkadii-morozov1@mail.ru>
2023-12-14 16:13:38 +0600

    added component: course-info

---
+++ resources/views/design-system/v4/components/molecules/course-info/index.blade.php
@@ -0,0 +1,247 @@
+<?php
+includeComponent('components/atoms/button');
+includeComponent('components/molecules/course-info');
+?>
+<div style="background: #f5f6f7; padding: 40px 0;">
+    <div class="container">
+
+        <div class="course-info">
+            <h2 class="h2 course-info_title">ТОП- <span>3</span> лучших онлайн-курсов по UX/UI-дизайну</h2>
+
+            <div class="course-info_wrapper">
+                <div class="course-info_table-title h4-desk">
+                    <span class="course-info_number">1</span>. Курс <a class="course-info_link" href="https://kursy.ru/" target="_blank">UX/UI-дизайнер (базовый)</a> от
+                    <span class="course-info_school">Международный Университет Профессиональной Переподготовки Подготовки и Заготовки</span>
+                </div>
+
+                <div class="course-info_content h-body-md">
+                    <p>Обучение навыкам создания дизайна сайтов и мобильных приложений.
+                        Стоимость курса указана по тарифу «Базовый», есть возможность выбрать другие тарифы:</p>
+                    <ul>
+                        <li>«Оптимальный» включает в себя все опции «Базового» тарифа, а также разбор портфолио, тестовое собеседование с экспертом в дизайне, индивидуальные консультации с ментором, групповые практические онлайн-воркшопы, карьерную консультацию, ревью резюме, курс по Adobe Illustrator.</li>
+                        <li>«VIP» включает в себя опции предыдущих тарифов, а также еженедельные персональные консультации с менторами, созвоны и ответы на любые вопросы по теории и практике, совместную работу с ментором над проектом, 2 индивидуальных консультации с топовыми экспертами рынка, дополнительную карьерную консультацию, курс по Cinema 4D.</li>
+                    </ul>
+                </div>
+
+                <table class="course-info_table h-body-md">
+                    <tbody>
+                    <tr class="tr">
+                        <td class="td col-first">Школа</td>
+                        <td class="td course-info_links col-second">
+                            Международный Университет Профессиональной Переподготовки Подготовки и Заготовки
+                            <a class="course-info_rating-reviews" href="https://kursy.ru/schools/geekbrains/#vote">
+                                <svg class="course-info_rating-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5.98625 16.175L7.0425 11.6088L3.5 8.53751L8.18 8.13126L10 3.82501L11.82 8.13126L16.5 8.53751L12.9575 11.6088L14.0137 16.175L10 13.7538L5.98625 16.175Z" fill="#141414"/>
+                                </svg>
+                                <span class="course-info_rating-count">3.97</span>
+                                <span class="course-info_reviews-value">(9999 отзывов)</span>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Длительность</td>
+                        <td class="td">8.5 месяцев</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Стоимость</td>
+                        <td class="td">113 400 ₽</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Цена в рассрочку</td>
+                        <td class="td">4 725 <span>₽/мес</span></td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Помощь в трудоустройстве</td>
+                        <td class="td">Есть</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Документ об окончании</td>
+                        <td class="td">Диплом о профессиональной переподготовке</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">
+                            График прохождения
+                        </td>
+                        <td class="td">
+                            Свободный график
+                        </td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Тип обучения</td>
+                        <td class="td">Профессия</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Доступ после прохождения</td>
+                        <td class="td">Есть</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Инструменты</td>
+                        <td class="td">Python, Matplotlib, PyTorch, Plotly, Keras, Airflow, Scikit-learn, Pandas, ClickHouse, Catboost, Docker, SQL, GitHub, NumPy</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Проекты в портфолио </td>
+                        <td class="td">Прототип мобильного приложения, страница сайта про архитектуру, главная страница сайта агентства недвижимости, прототип сайта или мобильного приложения онлайн-кинотеатра, интерактивный анимированный прототип мобильного приложения или сайта приложения (зависит от выбора специализации), дипломный проект.</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="course-info_wrapper">
+                <div class="course-info_table-title h4-desk">
+                    <span class="course-info_number">2</span>. Курс <a class="course-info_link" href="https://kursy.ru/" target="_blank">UX/UI-дизайнер (базовый)</a> от
+                    <span class="course-info_school">Международный Университет Профессиональной Переподготовки Подготовки и Заготовки</span>
+                </div>
+
+                <div class="course-info_content h-body-md">
+                    <p>Обучение навыкам создания дизайна сайтов и мобильных приложений.
+                        Стоимость курса указана по тарифу «Базовый», есть возможность выбрать другие тарифы:</p>
+                    <ul>
+                        <li>«Оптимальный» включает в себя все опции «Базового» тарифа, а также разбор портфолио, тестовое собеседование с экспертом в дизайне, индивидуальные консультации с ментором, групповые практические онлайн-воркшопы, карьерную консультацию, ревью резюме, курс по Adobe Illustrator.</li>
+                        <li>«VIP» включает в себя опции предыдущих тарифов, а также еженедельные персональные консультации с менторами, созвоны и ответы на любые вопросы по теории и практике, совместную работу с ментором над проектом, 2 индивидуальных консультации с топовыми экспертами рынка, дополнительную карьерную консультацию, курс по Cinema 4D.</li>
+                    </ul>
+                </div>
+
+                <table class="course-info_table h-body-md">
+                    <tbody>
+                    <tr class="tr">
+                        <td class="td col-first">Школа</td>
+                        <td class="td course-info_links col-second">
+                            Международный Университет Профессиональной Переподготовки Подготовки и Заготовки
+                            <a class="course-info_rating-reviews" href="https://kursy.ru/schools/geekbrains/#vote">
+                                <svg class="course-info_rating-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5.98625 16.175L7.0425 11.6088L3.5 8.53751L8.18 8.13126L10 3.82501L11.82 8.13126L16.5 8.53751L12.9575 11.6088L14.0137 16.175L10 13.7538L5.98625 16.175Z" fill="#141414"/>
+                                </svg>
+                                <span class="course-info_rating-count">3.97</span>
+                                <span class="course-info_reviews-value">(9999 отзывов)</span>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Длительность</td>
+                        <td class="td">8.5 месяцев</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Стоимость</td>
+                        <td class="td">113 400 ₽</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Цена в рассрочку</td>
+                        <td class="td">4 725 <span>₽/мес</span></td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Помощь в трудоустройстве</td>
+                        <td class="td">Есть</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Документ об окончании</td>
+                        <td class="td">Диплом о профессиональной переподготовке</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">
+                            График прохождения
+                        </td>
+                        <td class="td">
+                            Свободный график
+                        </td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Тип обучения</td>
+                        <td class="td">Профессия</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Доступ после прохождения</td>
+                        <td class="td">Есть</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Инструменты</td>
+                        <td class="td">Python, Matplotlib, PyTorch, Plotly, Keras, Airflow, Scikit-learn, Pandas, ClickHouse, Catboost, Docker, SQL, GitHub, NumPy</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Проекты в портфолио </td>
+                        <td class="td">Прототип мобильного приложения, страница сайта про архитектуру, главная страница сайта агентства недвижимости, прототип сайта или мобильного приложения онлайн-кинотеатра, интерактивный анимированный прототип мобильного приложения или сайта приложения (зависит от выбора специализации), дипломный проект.</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="course-info_wrapper">
+                <div class="course-info_table-title h4-desk">
+                    <span class="course-info_number">3</span>. Курс <a class="course-info_link" href="https://kursy.ru/" target="_blank">UX/UI-дизайнер (базовый)</a> от
+                    <span class="course-info_school">Международный Университет Профессиональной Переподготовки Подготовки и Заготовки</span>
+                </div>
+
+                <div class="course-info_content h-body-md">
+                    <p>Обучение навыкам создания дизайна сайтов и мобильных приложений.
+                        Стоимость курса указана по тарифу «Базовый», есть возможность выбрать другие тарифы:</p>
+                    <ul>
+                        <li>«Оптимальный» включает в себя все опции «Базового» тарифа, а также разбор портфолио, тестовое собеседование с экспертом в дизайне, индивидуальные консультации с ментором, групповые практические онлайн-воркшопы, карьерную консультацию, ревью резюме, курс по Adobe Illustrator.</li>
+                        <li>«VIP» включает в себя опции предыдущих тарифов, а также еженедельные персональные консультации с менторами, созвоны и ответы на любые вопросы по теории и практике, совместную работу с ментором над проектом, 2 индивидуальных консультации с топовыми экспертами рынка, дополнительную карьерную консультацию, курс по Cinema 4D.</li>
+                    </ul>
+                </div>
+
+                <table class="course-info_table h-body-md">
+                    <tbody>
+                    <tr class="tr">
+                        <td class="td col-first">Школа</td>
+                        <td class="td course-info_links col-second">
+                            Международный Университет Профессиональной Переподготовки Подготовки и Заготовки
+                            <a class="course-info_rating-reviews" href="https://kursy.ru/schools/geekbrains/#vote">
+                                <svg class="course-info_rating-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5.98625 16.175L7.0425 11.6088L3.5 8.53751L8.18 8.13126L10 3.82501L11.82 8.13126L16.5 8.53751L12.9575 11.6088L14.0137 16.175L10 13.7538L5.98625 16.175Z" fill="#141414"/>
+                                </svg>
+                                <span class="course-info_rating-count">3.97</span>
+                                <span class="course-info_reviews-value">(9999 отзывов)</span>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Длительность</td>
+                        <td class="td">8.5 месяцев</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Стоимость</td>
+                        <td class="td">113 400 ₽</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Цена в рассрочку</td>
+                        <td class="td">4 725 <span>₽/мес</span></td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Помощь в трудоустройстве</td>
+                        <td class="td">Есть</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Документ об окончании</td>
+                        <td class="td">Диплом о профессиональной переподготовке</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">
+                            График прохождения
+                        </td>
+                        <td class="td">
+                            Свободный график
+                        </td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Тип обучения</td>
+                        <td class="td">Профессия</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Доступ после прохождения</td>
+                        <td class="td">Есть</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Инструменты</td>
+                        <td class="td">Python, Matplotlib, PyTorch, Plotly, Keras, Airflow, Scikit-learn, Pandas, ClickHouse, Catboost, Docker, SQL, GitHub, NumPy</td>
+                    </tr>
+                    <tr class="tr">
+                        <td class="td">Проекты в портфолио </td>
+                        <td class="td">Прототип мобильного приложения, страница сайта про архитектуру, главная страница сайта агентства недвижимости, прототип сайта или мобильного приложения онлайн-кинотеатра, интерактивный анимированный прототип мобильного приложения или сайта приложения (зависит от выбора специализации), дипломный проект.</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
